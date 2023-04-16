88885e47a45d5e93aaf6814a11ccf515ca56db89
barbaracarvajal <barb.carvajalsaez@gmail.com>
2023-04-15 20:46:35 -0400

    codigo parte 1

---
+++ parte1.py
@@ -0,0 +1,89 @@
+#comentario de una linea
+
+"""comentario de 
+multiples 
+lineas"""
+
+# print('Holi mundo ;D')
+
+variable = "hola cxzc _ . ! 415634 23.0"
+#print(variable)
+variable = "chao"
+#print(variable)
+
+#numeros
+numero = 12 # int
+numeroNegativo = -2 # int
+numeroComplejo = 2+5j # complex
+numeroDecimal = 1.3 #float
+
+"""
+print(type(variable))
+print(type(numero))
+print(type(numeroNegativo))
+print(type(numeroComplejo))
+print(type(numeroDecimal))
+""" 
+
+# cadena de texto
+
+texto = "hola como estas :p" # str string
+
+# print(type(texto))
+
+#booleanos
+
+#True False
+
+#Listas
+
+lista = [12.3, "hola", True, False, 23]
+print(type(lista))
+
+# Tuplas
+
+tupla = (12.3, "hola", True, False, 23)
+print(type(tupla))
+
+# Diccionarios
+
+diccionario = {
+    "nombre": "mokita",
+    "edad": 4,
+    "peso": 3.8
+}
+
+# oporaciones con cadenas
+
+#concatenacion (+)
+
+palabra1 = "prima"
+palabra2 = "vera"
+palabra3 = palabra1 + palabra2
+# print(palabra3)
+
+#multiplicacion *
+
+palabra = "python"
+mutiPalabra = palabra * 3
+# print(mutiPalabra)
+
+
+# Operadores matematicos
+
+suma = 5 + 6
+resta = 5 - 6
+division = 5 / 6
+division2 = 5//6 #ignorara la francion
+multiplicacion = 5*6
+modulo = 5%6
+potencia = 5**6
+
+
+print(f"suma:{suma}")
+print(f"resta:{resta}")
+print(f"division:{division}")
+print(f"division2:{division2}")
+print(f"multiplicacion:{multiplicacion}")
+print(f"modulo:{modulo}")
+print(f"potencia:{potencia}")
